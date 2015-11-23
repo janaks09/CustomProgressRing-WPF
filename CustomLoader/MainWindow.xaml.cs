d1c648c80fec3d99496d1586f3a89e4b1317da35
Janak Shrestha <old_ink@hotmail.com>
2015-11-23 20:19:59 +0545

    custom progress ring-WPF

---
+++ CustomLoader/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomLoader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void loaderPlay_Click(object sender, RoutedEventArgs e)
+        {
+            Loader.Visibility = Visibility.Visible;
+        }
+
+        private void loaderStop_Click(object sender, RoutedEventArgs e)
+        {
+            Loader.Visibility = Visibility.Collapsed;
+        }
+    }
+}
